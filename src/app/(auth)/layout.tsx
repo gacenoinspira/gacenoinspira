71d5e81913d96b4f137d1c69ea4ffa3c00750b14
camilo guzman <jeysonkmguzman@gmail.com>
2025-06-18 21:19:41 -0500

    layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { UiHeader } from "../(public)/components";
 import styles from "./layout.module.css";
+import { getInfoUser } from "@/lib/action";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getInfoUser();
   return (
     <main className={styles.main}>
-      <UiHeader />
+      <UiHeader userDb={user.data} />
       {children}
     </main>
   );
