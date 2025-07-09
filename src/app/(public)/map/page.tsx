b900a3b1fee9413935c81ceecec47db0ea25495a
camilo guzman <jeysonkmguzman@gmail.com>
2025-07-09 12:10:56 -0500

    Fix: editar

--- src/app/(public)/map/page.tsx
+++ src/app/(public)/map/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Navigate } from "./navigate/navigate";
+import styles from "./page-map.module.css";
 
 export default function PageMap() {
   return (
-    <div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        DESCUBRE CÓMO LLEGAR A SAN LUIS DE GACENO
+      </h1>
       <Navigate />
     </div>
   );
