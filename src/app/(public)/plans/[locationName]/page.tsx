0dbe6216515e318744f23e736638ed6384155d34
aldemar niño cortazar <anino79@misena.edu.co>
2025-08-13 16:43:48 -0500

    add space

--- src/app/(public)/plans/[locationName]/page.tsx
+++ src/app/(public)/plans/[locationName]/page.tsx
@@ -144,7 +144,7 @@ export default function DynamicPlanificaPage() {
         </div>
       )}
 
-      {userLocation && (
+      {userLocation  && (
         <MapaDestino
           origen={userLocation}
           destino={{
